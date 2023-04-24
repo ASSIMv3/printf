4ba4823148df3eb5145b94bd3bc2134f8f34b989
Sana Assim <sanaassim1@gmail.com>
2023-04-24 11:53:53 -0700

    Convert an int to binary

--- imprime_numbers.c
+++ imprime_numbers.c
@@ -44,3 +44,38 @@ int imprime_i_d(va_list given_args, int *flags)
 	}
 	return (length);
 }
+
+/**
+ * imprime_binary - a function that prints an unsigned integer
+ *
+ * @given_args: a list of a given arguments.
+ * @flags: list of flags
+ * Return: the size of the number.
+ */
+int imprime_binary(va_list given_args, __attribute__((unused)) int *flags)
+{
+	unsigned int num = va_arg(given_args, unsigned int);
+	int binary[32];
+	int printed_chars, i, j;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	i = 0;
+	printed_chars = 0;
+	while (num > 0)
+	{
+		binary[i] = num % 2;
+		num /= 2;
+		i++;
+		printed_chars++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(binary[j] + '0');
+
+	return (printed_chars);
+}
