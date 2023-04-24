063c9f9b5f5a752a5eb38f8f619986c702701da1
ASSIMv3 <hassanassim01@gmail.com>
2023-04-24 04:38:12 -0700

    update _putchar

--- _putchar.c
+++ _putchar.c
@@ -7,7 +7,17 @@
  *
  * Return: 1
  */
-int _putchar(char c)
+int _putchar(char character)
 {
-	return (write(1, &c, 1));
+	static char buffer[1024];
+	static int buff_size;
+
+	if (character == -1 || buff_size >= 1024)
+	{
+		write(1, &buffer, buff_size);
+		buff_size = 0;
+	}
+	if (character != -1)
+		buffer[buff_size++] = character;
+	return (1);
 }
