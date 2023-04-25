4c42a50168f389d4ccf315051aa7c3c891a6eb5d
ASSIMv3 <hassanassim01@gmail.com>
2023-04-25 02:21:03 -0700

    Add print String function

--- imprime_arg.c
+++ imprime_arg.c
@@ -18,11 +18,12 @@ int (*imprime_arg(char c))(va_list, int *)
 		{'u', imprime_unsigned},
 		{'o', imprime_octal},
 		{'x', imprime_hex_lower},
-		{'X', imprime_hex_upper}
+		{'X', imprime_hex_upper},
+		{'S', imprime_String}	
 	};
 	int i = 0;
 
-	while (i < 10)
+	while (i < 11)
 	{
 		if (c == data_types[i].c)
 			return (data_types[i].func);
